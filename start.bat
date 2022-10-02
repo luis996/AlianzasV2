d3a755b9cd4809d66f0ebfa683bf7356fb2fd148
luis996 <52387429+luis996@users.noreply.github.com>
2022-10-01 19:31:58 -0500

    first commit

---
+++ start.bat
@@ -0,0 +1,10 @@
+@echo off
+cls
+echo.
+echo    Advertencia: vas a necesitar node.js instalado para poder continuar,
+echo    presiona una tecla para abrir aplicación
+echo Si no inicia es porque necesitas instalar node.js buscalo en google
+echo desmarca "install the necessary tools" al instalar
+pause>nul
+echo iniciando...
+electron .
