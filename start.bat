1e06faeaafb6850b43e59233eb7c42390e6da32e
luis996 <52387429+luis996@users.noreply.github.com>
2022-10-01 23:03:05 -0500

    Add necesary tools

--- start.bat
+++ start.bat
@@ -7,4 +7,8 @@ echo Si no inicia es porque necesitas instalar node.js buscalo en google
 echo desmarca "install the necessary tools" al instalar
 pause>nul
 echo iniciando...
-electron .
+npm install
+echo Herramientas necesarias instaladas, por favor presione de nuevo
+pause>nul
+echo iniciando...
+node main.js
